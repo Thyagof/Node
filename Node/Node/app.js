205699b51c1bdf8aec8eea84628adf00785c226f
Thyagof <thyago.abfc@gmail.com>
2016-02-27 13:08:42 -0300

    Created server and new modules

--- Node/Node/app.js
+++ Node/Node/app.js
@@ -1,1 +1,11 @@
-﻿console.log('Hello world');
+﻿var http = require('http'), 
+    path = require('path'),
+    fs = require('fs'),
+    index = fs.readFileSync('./public/index.html')
+
+http.createServer(function (request, response) {
+    response.writeHead(200, { 'Content-Type': 'text-html' });
+    response.end;
+}).listen(8181, function () {
+    console.log('Teste no 8181');
+});
---
+++ Node/Node/lib/user/index.js
@@ -0,0 +1,20 @@
+﻿var Usuario = function Usuario(){
+    var _autorizado = false;
+    var _usuario = undefined;
+    var _senha = undefined;
+
+    var autorizar = function (usuario, senha){
+        _usuario = usuario;
+        _senha = senha;
+        // Executar lógica de validação
+        _autorizado = true
+        return true;
+    }
+
+    return {
+        autorizar: autorizar,
+        autorizado: function(){ return autorizado; }
+    }
+}
+
+module.exports = Usuario;
